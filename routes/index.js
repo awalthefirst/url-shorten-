4269c6a5b22894634c4ab6a1d8d4ef51fd2e712f
Abdul   Awali <abdulrazakawal@gmail.com>
2016-01-01 07:30:54 +0000

    intial

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var TinyURL = require('tinyurl');
+var validUrl = require('valid-url');
+var urlParse = require('url');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+   if (req.query.s === undefined) {
+      res.render('index', {
+         title: 'url-shortener'
+      });
+
+   }
+   else {
+      var url = req.query.s;
+      res.redirect('http://tinyurl.com/'+url);
+ 
+   }
+
+});
+
+
+
+router.get('/short', function (req, res, next) {
+
+   var url = req.query.s || 'hhhh';
+  
+   if (validUrl.isUri(url)) {
+      TinyURL.shorten('http://google.com', function (data) {
+         
+        res.json({
+           "original_url":url,
+           "short_url":'fcc-short'+urlParse.parse(data).pathname
+        }); 
+         
+      });
+      
+   }
+   else {
+      res.json({
+         "error": true,
+         "code": "not valid url"
+      });
+   }
+
+});
+
+module.exports = router;
